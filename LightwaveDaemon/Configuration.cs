6df3f4d90657ed15e577d80354d9016a37f0144c
James Housley <jh-bitbucket@drowl.co.uk>
2025-02-08 22:16:19 +0000

    Clean up Configuration syntax

--- LightwaveDaemon/Configuration.cs
+++ LightwaveDaemon/Configuration.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace LightwaveDaemon
 {
-    internal enum DeviceName
+    internal enum DeviceName //Add descriptions for any devices set up in Lightwave with a name containing spaces 
     {
         [Description("Hallway light")]
         Hallway,
@@ -16,27 +16,30 @@ namespace LightwaveDaemon
         public static string EmailFromAddress = "address@domain.com"; 
         public static string EmailToAddress = "address@domain.com";
 
-        public static Phone[] Phones = new Phone[]
-        {
+        public static Phone[] Phones =
+        [
             new Phone("PERSON NAME", "PHONE NAME", "OPTIONAL FIXED PHONE IP")
-        };
+        ];
 
         public static TimeSpan BedTime => TimeSpan.Parse("23:11"); //The application currently relies on this time being before midnight
 
+        private static readonly bool on = true;
+        private static readonly bool off = false;
+        private static readonly bool evenIfHome = true;
 
-        private static List<DaemonAutomation> _daemonAutomations = new List<DaemonAutomation>
-        {
+        private static readonly List<DaemonAutomation> _daemonAutomations =
+        [
             //Each automation needs an "example time" which assumes a dusk time of 6pm and a bed time of 11pm. These
             //will then be converted into real times later depending on the real dusk and bed times.
-            new DaemonAutomation("Dusk", TimeSpan.Parse("18:00"), new StateChange[]
-            {
-                new StateChange(DeviceName.Hallway, true, true), //Always turn on hallway light, regardless of whether anyone is home or not
-            }),
-            new DaemonAutomation("Hallway off", TimeSpan.Parse("22:45"), new StateChange[]
-            {
-                new StateChange(DeviceName.Hallway, false) //Only turn light off if no-one is home
-            })
-        };
+            new("Dusk", TimeSpan.Parse("18:00"),
+            [
+                new StateChange(DeviceName.Hallway, on, evenIfHome), //Always turn on hallway light, regardless of whether anyone is home or not
+            ]),
+            new("Hallway off", TimeSpan.Parse("22:45"),
+            [
+                new StateChange(DeviceName.Hallway, off) //Only turn light off if no-one is home
+            ])
+        ];
 
         public static IEnumerable<DaemonAutomation> DaemonAutomations => _daemonAutomations.OrderBy(x => x.ExampleTime);
     }
